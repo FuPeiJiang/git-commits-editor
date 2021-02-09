32b13e1b227a3bf30550a5e0aafe6704f02e26b2
FuPeiJiang <42662615+FuPeiJiang@users.noreply.github.com>
2021-02-08 19:22:53 -0500

    remove comments in src/CodelensProvider.ts

--- src/CodelensProvider.ts
+++ src/CodelensProvider.ts
@@ -1,9 +1,4 @@
 import { CodeLensProvider, TextDocument, CodeLens, workspace } from 'vscode'
-import { CallbackIfEval, CallBackUntilOtherEval } from './createRunThrough'
-// import * as vscode from 'vscode'
-/**
- * CodelensProvider
- */
 
 type FuncAnyReturnCodeLensArr = (...args: any[]) => CodeLens[]
 export type { FuncAnyReturnCodeLensArr }
@@ -11,17 +6,8 @@ export type { FuncAnyReturnCodeLensArr }
 export class CodelensProvider implements CodeLensProvider {
   getRanges: FuncAnyReturnCodeLensArr
 
-  // private regex: RegExp
-  // private _onDidChangeCodeLenses: vscode.EventEmitter<void> = new vscode.EventEmitter<void>()
-  // public readonly onDidChangeCodeLenses: vscode.Event<void> = this._onDidChangeCodeLenses.event
-
   constructor(getRanges: FuncAnyReturnCodeLensArr) {
     this.getRanges = getRanges
-    // this.regex = /(.+)/g
-
-  //   workspace.onDidChangeConfiguration(() => {
-  // this._onDidChangeCodeLenses.fire()
-  //   })\
   }
 
   public provideCodeLenses(document: TextDocument): CodeLens[] {
@@ -32,19 +18,4 @@ export class CodelensProvider implements CodeLensProvider {
     return []
   }
 
-/*   public resolveCodeLens(codeLens: CodeLens): CodeLens|null {
-  // public resolveCodeLens(codeLens: CodeLens, token: vscode.CancellationToken) {
-    if (workspace.getConfiguration('codelens-sample').get('enableCodeLens', true)) {
-      console.log(codeLens.range)
-
-      codeLens.command = {
-        title: 'Codelens provided by sample extension',
-        command: 'codelens-sample.codelensAction',
-        arguments: 'Argument 1',
-      }
-      return codeLens
-    }
-    return null
-  }
-  */
 }
